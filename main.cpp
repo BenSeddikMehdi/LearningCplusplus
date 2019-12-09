713779ca39cbb7a21c930156ab18ce6b3c6ba108
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-10 00:35:54 +0100

    Getting started with C++

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hola Amigos" << endl;
+    cout << "Min of int " << numeric_limits <unsigned int>::min() << endl;
+    cout << "Max of int " << numeric_limits <unsigned int>::max() << endl;
+
     return 0;
 }
