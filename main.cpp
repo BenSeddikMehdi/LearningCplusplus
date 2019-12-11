cd72ce8715dffb70496934d6a5ae13f1420e18c8
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-11 02:13:01 +0100

    stoi(x) converting the string x into integer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <limits>
+#include <cmath>
 
 using namespace std;
 
@@ -9,9 +10,18 @@ int main() {
     cin >> a;
     cout << "Enter another number: " << endl;
     cin >> b;
-    int aa = 0, bb = 0;
+    double aa = 0, bb = 0;
+    int na = a.size(), nb = b.size();
+    cout << pow(10, 3);
+    for (int i = 0; i < na; ++i) {
+        aa += (a[i]-'0')*pow(10, na-1-i);
+    }
+    for (int j = 0; j < nb; ++j) {
+        bb += (b[j]-'0')*pow(10, nb-j-1);
+    }
+    cout << "Long method: " << aa + bb << endl;
     aa = stoi(a);
     bb = stoi(b);
-    cout << aa + bb << endl;
+    cout << "short method " << aa + bb << endl;
     return 0;
 }
