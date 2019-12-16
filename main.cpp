7fa885268653ca7143d0865428ccabbfd5f54e08
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-16 17:47:32 +0100

    Vectors in C++

--- main.cpp
+++ main.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
 #include <limits>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    cout << "Enter a number: " << endl;
-    string a, b;
-    cin >> a;
-    cout << "Enter another number: " << endl;
-    cin >> b;
-    double aa = 0, bb = 0;
-    int na = a.size(), nb = b.size();
-    cout << pow(10, 3);
-    for (int i = 0; i < na; ++i) {
-        aa += (a[i]-'0')*pow(10, na-1-i);
+    vector <uint16_t> randomVector(3);
+    for (int i = 0; i < (int) randomVector.size(); ++i) {
+        randomVector[i] = i*i + 1;
+        printf("randomVector[%d] = %d\n", i, randomVector[i]);
     }
-    for (int j = 0; j < nb; ++j) {
-        bb += (b[j]-'0')*pow(10, nb-j-1);
-    }
-    cout << "Long method: " << aa + bb << endl;
-    aa = stoi(a);
-    bb = stoi(b);
-    cout << "short method " << aa + bb << endl;
+    randomVector.push_back(14);
+    printf("the last item inside the vector: %d", randomVector[randomVector.size()-1]);
     return 0;
 }
